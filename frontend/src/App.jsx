d3c437343ef56afa0cee69a01c15e1ab4a6116c9
phANTom2303 <anikavy.goenka@gmail.com>
2025-05-19 13:45:18 +0530

    configured tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from "./components/header/Header";
 function App() {
-
   return (
     <>
-    <h1>Hellow World !</h1>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hellow World !</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
