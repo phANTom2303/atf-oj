13d71138fee4d16c8e9db1837260fdfe26cdc2e0
phANTom2303 <anikavy.goenka@gmail.com>
2025-05-19 13:37:41 +0530

    initialized front-end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+    <h1>Hellow World !</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="heading"> Ready to Code</div>
+        <div className="menu"> 
+            <ul>
+                <li> Problem List</li>
+                <li> Online Judge</li>
+            </ul>
+        </div>
+      </div>
+    </>
+  );
+}
